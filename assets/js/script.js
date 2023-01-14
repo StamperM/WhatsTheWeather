831d6d3abb10b64d727da558bd84580df5ced3c6
Melissa <90412072+StamperM@users.noreply.github.com>
2023-01-13 22:40:59 -0500

    made some JS and HTML changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
+var cityName = document.querySelector("#cityName")
+
 // capture user input for city
 // get city from local storage and (have to get which grab the mc'ds)
 // set city to local storeage(after you get you set, )
 // append to search list
 // fetch geolocation
+fetch("http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}")
+.then(function(response)){
+return response
+})
+then(function(data)){
+    console.log(data);
+}
 // fetch weather
 // populate weather for today's date -icon,temp, humidity,wind speed
 // fetch the api https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
