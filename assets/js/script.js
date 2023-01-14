b4762ecd9f66882e0da07f4d456ee8567a4e2c0b
Melissa <90412072+StamperM@users.noreply.github.com>
2023-01-13 22:03:52 -0500

    set up

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// capture user input for city
+// get city from local storage and (have to get which grab the mc'ds)
+// set city to local storeage(after you get you set, )
+// append to search list
+// fetch geolocation
+// fetch weather
+// populate weather for today's date -icon,temp, humidity,wind speed
+// fetch the api https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// populate weather for next 5 days - icon,temp, humidity,wind speed
+// using search list populate current and future. 
+// create get data function 
