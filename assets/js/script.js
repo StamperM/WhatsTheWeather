b61dcfafe8a93b8585289300dcf52d351b95a575
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-01-23 09:46:26 -0500

    updated eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,61 @@ const apiKey = "d5b935342e8b14cd96e3c89d3209dc4a";
 const weatherApiKey = "283f7d6c1e685eac6be05c60305a774a";
 const fiveDayApiKey = "bb8a3d984c401bd3e0b7c5b109985c9e";
 // get city from local storage 
-var storedWeather = JSON.parse(localStorage.getItem("city"))|| [];
+var storedWeather = JSON.parse(localStorage.getItem("city")) || [];
 let currentWeather;
 const dailyIcon = document.getElementById("dailyIcon");
 const dailyTemp = document.getElementById("dailyTemp");
 const dailyHumidity = document.getElementById("dailyHumidity");
 const dailyWind = document.getElementById("dailyWind");
-const football = {     
+let userSelectedCity;
+const football = {
     city: {},
     daily: {},
     fiveDay: [],
 }
-const citySearchbutton = document.getElementsByClassName("citySearch");
+
 const button = document.getElementById("citySearch");
 todayDate = document.getElementById("todayDateHTML").innerHTML = (dayjs().format("MMM D, YYYY"));
+
 button.addEventListener("click", getLatAndLong);
+
 displayCityData();
-citySearchbutton.addEventListener("click",searchfromlist);
+
+// This allows user to select a previouly searched city to be searched for again by clicking the name. 
+
+document.querySelectorAll(".golf").forEach((city) => {
+    city.addEventListener("click", (event) => {
+        cityName = event.target.textContent;
+        getLatAndLong();
+    })
+}) 
 
 
-function searchfromlist(){
-    preventDefault();
-    getLatAndLong();
+
+
+function getLatAndLong(){
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=&appid=" + apiKey)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+        football.city = cityInput;
+        getWeatherCurrent(data[0].lat, data[0].lon);
+        getFiveDayWeather(data[0].lat, data[0].lon);
+
+        if (storedWeather.indexOf(cityInput) === -1) {
+            storedWeather.push(cityInput);
+            localStorage.setItem("city", JSON.stringify(storedWeather));
+        }
+        console.log("football", football);
+        displayCityData();
+
+    });
+
+
 }
+
 
 
 
@@ -34,7 +66,7 @@ function searchfromlist(){
 
 // fetch geolocation
 function getLatAndLong() {
-  
+
     cityInput = (cityName.value);
 
     // console.log(cityInput);
@@ -47,16 +79,16 @@ function getLatAndLong() {
         .then(function (data) {
             console.log(data);
             football.city = cityInput;
-                getWeatherCurrent(data[0].lat, data[0].lon);
+            getWeatherCurrent(data[0].lat, data[0].lon);
             getFiveDayWeather(data[0].lat, data[0].lon);
-            
-            if(storedWeather.indexOf(cityInput)=== -1){
+
+            if (storedWeather.indexOf(cityInput) === -1) {
                 storedWeather.push(cityInput);
-            localStorage.setItem("city", JSON.stringify(storedWeather));
+                localStorage.setItem("city", JSON.stringify(storedWeather));
             }
-            console.log("football",football);
+            console.log("football", football);
             displayCityData();
-            
+
         });
 
 
@@ -73,14 +105,14 @@ function getWeatherCurrent(lat, long) {
             console.log(data);
 
             console.log(data.weather[0].icon);
-            
+
             football.daily = {
                 cityNameDaily: data.name,
-                weatherIcon:data.weather[0].icon,
+                weatherIcon: data.weather[0].icon,
                 dailyTemp: data.main.temp,
                 dailyHumidity: data.main.humidity,
                 dailyWind: data.wind.speed,
-                
+
             }
             displayCurrentWeather();
             //     dailyTemp.textContent = `Tempature: ${data.main.temp}`;
@@ -102,28 +134,29 @@ function getFiveDayWeather(lat, long) {
         .then(function (data) {
             football.fiveDay = [];
             console.log(data);
-            const fivedays = [data.list[1],data.list[9],data.list[17],data.list[25],data.list[33]];
-            fivedays.forEach((everyday)=>{
-            football.fiveDay.push( {
-                date: everyday.dt_txt,
-                fiveDayweatherIcon: everyday.weather[0].icon,
-                tempature: everyday.main.temp,
-                windSpeed: everyday.wind.speed,
-                humidity: everyday.main.humidity,
+            const fivedays = [data.list[1], data.list[9], data.list[17], data.list[25], data.list[33]];
+            fivedays.forEach((everyday) => {
+                football.fiveDay.push({
+                    date: everyday.dt_txt,
+                    fiveDayweatherIcon: everyday.weather[0].icon,
+                    tempature: everyday.main.temp,
+                    windSpeed: everyday.wind.speed,
+                    humidity: everyday.main.humidity,
+                })
             })
-        })
             displayFiveDay()
         })
 }
 
 function displayCityData() {
     const soccer = document.getElementById('soccer');
-    soccer.innerHTML='';
+    soccer.innerHTML = '';
     let template = "<ul>"
     storedWeather.forEach((city) => {
         template +=
-            ` <li class="citySearch"><button class="citySearch">${city}</button></li>`
+            ` <li class="golf">${city}</li>`
     })
+    // <button class=" buttonCity" id ="${city}></button>
     template += `</ul>`
     console.log(soccer);
     console.log(template);
@@ -131,18 +164,18 @@ function displayCityData() {
 }
 
 
-function displayCurrentWeather(){
-    console.log('display footbal',football);
-   
-const containerTodaysWeather = document.getElementById("currentWeatherDiv");
-let todaysWeather = "<div>";
-containerTodaysWeather.innerHTML ="";
-todaysWeather += `
-<div>
+function displayCurrentWeather() {
+    console.log('display footbal', football);
+
+    const containerTodaysWeather = document.getElementById("currentWeatherDiv");
+    let todaysWeather = "<div>";
+    containerTodaysWeather.innerHTML = "";
+    todaysWeather += `
+<div class="currentSectionONe>
 <h2 class="dailyH2">${football.city}</h2>
 <h2 class="dailyH2"> ${todayDate}</h2 >
 </div>
-<div>
+<div class= currentSectionTwo>
 <div id="dailyWeatherIcon"> <img src="./assets/icons/${football.daily.weatherIcon}.png" alt=""></div>
   <ul>
 <li id="dailyTemp">Tempature: ${football.daily.dailyTemp}</li>
@@ -150,21 +183,21 @@ todaysWeather += `
   <li id="dailyWind">Wind Speeds: ${football.daily.dailyWind}</li>
 </ul>
   </div>`
-console.log("todays Weather",todaysWeather);
-console.log("container",containerTodaysWeather);
+    console.log("todays Weather", todaysWeather);
+    console.log("container", containerTodaysWeather);
 
-todaysWeather += `</div>`;
-containerTodaysWeather.insertAdjacentHTML("beforeend", todaysWeather);
+    todaysWeather += `</div>`;
+    containerTodaysWeather.insertAdjacentHTML("beforeend", todaysWeather);
 }
 
-function displayFiveDay(){
-    console.log("fiveDar",football);
-    const aCard= document.getElementById("fiveday");
+function displayFiveDay() {
+    console.log("fiveDar", football);
+    const aCard = document.getElementById("fiveday");
     // console.log("a card", weatherCards);
-    aCard.innerHTML="";
-let eachDay = "<div class= colCards>"
-football.fiveDay.forEach((everyday)=>{
-    eachDay +=`
+    aCard.innerHTML = "";
+    let eachDay = "<div class= colCards>"
+    football.fiveDay.forEach((everyday) => {
+        eachDay += `
 <h2>${everyday.date}</h2>
   <ul>
     <li><img src="./assets/icons/${everyday.fiveDayweatherIcon}.png"</li>
@@ -172,10 +205,10 @@ football.fiveDay.forEach((everyday)=>{
    <li>Wind Speed: ${everyday.windSpeed}</li>
     <li>Humidity: ${everyday.humidity}</li>
   </ul>`
-})
+    })
 
-eachDay += "</div>"
-aCard.insertAdjacentHTML("beforeend",eachDay);
+    eachDay += "</div>"
+    aCard.insertAdjacentHTML("beforeend", eachDay);
 }
 
 
